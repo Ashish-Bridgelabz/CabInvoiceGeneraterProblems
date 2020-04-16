ade3167af8c2f8969c5538b0ea7de1d4a12481e1
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-16 23:55:45 +0530

    ADDED:Ride class to store all detail of ride

---
+++ main/java/com/bridgelabz/service/Ride.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.service;
+
+public class Ride {
+    public double distance;
+   public double time;
+
+    public Ride(double distance, int time)
+    {
+        this.distance = distance;
+        this.time = time;
+    }
+}

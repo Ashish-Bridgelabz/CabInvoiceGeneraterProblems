fdd778d7353371b71782c2d6a0671a9a0ce1193b
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-17 19:02:32 +0530

    ADDED:Store user id and number of rides

--- main/java/com/bridgelabz/service/InvoiceService.java
+++ main/java/com/bridgelabz/service/InvoiceService.java
@@ -1,6 +1,5 @@
 package com.bridgelabz.service;
 
-
 public class InvoiceGenerator {
     private static final int COST_PER_TIME = 1;
     private static final double MINIMUM_COST_PER_KILOMETER = 10;
@@ -11,16 +10,24 @@ public class InvoiceGenerator {
         return Math.max(totalFare, MINIMUM_FARE);
     }
 
-
     public InvoiceSummary calculatefare(Ride[] rides) {
         double totalFare = 0;
         for (Ride ride : rides)
             totalFare += this.calculatefare(ride.distance, (int) ride.time);
 
-        return new InvoiceSummary(rides.length,totalFare);
+        return new InvoiceSummary(rides.length, totalFare);
     }
 
-  }
+    public void addRides(String userId, Ride[] rides) {
+        rideRepository.addRides(userId,rides);
+    }
+
+    @Override
+    public InvoiceSummary getInvoiceSummary(String userId) {
+        double totalFare = this.calculateFare(rideRepository.getRides(userId));
+        return new InvoiceSummary(rideRepository.getRides(userId).length,totalFare);
+    }
+}
 
 
 

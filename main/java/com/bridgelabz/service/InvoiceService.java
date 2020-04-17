d4854e50e10a86de27f95c6601ccca57f81d2b5d
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-17 21:57:53 +0530

    ADDED:Enum to support two categories of rides

--- main/java/com/bridgelabz/service/InvoiceService.java
+++ main/java/com/bridgelabz/service/InvoiceService.java
@@ -3,11 +3,27 @@ package com.bridgelabz.service;
 import java.util.ArrayList;
 
 public class InvoiceService {
-    private static final int COST_PER_TIME = 1;
-    private static final double MINIMUM_COST_PER_KILOMETER = 10;
-    private static final double MINIMUM_FARE = 5;
+    private  int COST_PER_TIME;
+    private  double MINIMUM_COST_PER_KILOMETER;
+    private double MINIMUM_FARE;
+
+    public enum subscriptionPlan {PremimumRides, NormalRides}
+
     RideRepository rideRepository = new RideRepository();
     ArrayList<Ride> listOfRides = new ArrayList<Ride>();
+
+    public InvoiceService(InvoiceService.subscriptionPlan plan) {
+        if (plan.equals(InvoiceService.subscriptionPlan.PremimumRides)) {
+            this.MINIMUM_FARE = 15;
+            this.MINIMUM_COST_PER_KILOMETER = 20;
+            this.COST_PER_TIME = 2;
+        }
+        if (plan.equals(InvoiceService.subscriptionPlan.NormalRides)) {
+            this.MINIMUM_FARE = 5;
+            this.MINIMUM_COST_PER_KILOMETER = 10;
+            this.COST_PER_TIME = 1;
+        }
+    }
     public double calculatefare(double distance, int time) {
         double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
         return Math.max(totalFare, MINIMUM_FARE);
